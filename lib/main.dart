fb7018bb31d63f0b4f1b79ce35eee79c9829f2f7
Andres Tamayo Martinez <19030296@itcelaya.edu.mx>
2024-04-17 10:11:05 -0600

    Creacion del SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pmsn_07/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pmsn_07/screens/splash_screen.dart';
+import 'package:pmsn_07/screens/dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,14 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Proyecto Final'),
-        ),
-        body: const Center(
-          child: Text('Hola Mundo'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      routes: {
+        "/dash": (BuildContext context) => const DashboardScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, "/dash");
+    });
+  }
+
+  @override
+  void dispose(){
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromRGBO(246, 237, 220, 1), Color.fromRGBO(88, 104, 117, 1)],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/logo.png'),
+            Text(
+              'ChatLynx',
+              style: TextStyle(
+                color: Colors.grey[300],
+                fontSize: 40,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
