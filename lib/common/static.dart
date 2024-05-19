dc1734b497bf497b40adc098a0211d3eee80c2b1
Leonardo Covarrubias Lemus <leonardolemus216@gmail.com>
2024-05-19 17:56:07 -0600

    Nueva clave zego

--- lib/common/static.dart
+++ lib/common/static.dart
@@ -1,5 +1,5 @@
 class Statics {
   static const String appSign =
-      '4840fba6edeec1ba8805d12b488b3b60a768b163631d39b1417d0f0ef7b9f1f7';
-  static const int appID = 1664044469; //Your appID
+      '446a92a3f0538681548375b537598280c22834dc6802f6fe9395a3091db251ea';
+  static const int appID = 903922125; //Your appID
 }
